47a89f6a79fe3b75b7c8a81bee66fed542ab3c5f
Theoadal <theoadal@gmail.com>
2025-07-20 18:11:52 +0300

    changes parameter to id instead of username

--- middleware/isOwner.js
+++ middleware/isOwner.js
@@ -1,9 +1,9 @@
 // middleware/isOwner.js
 
 module.exports = function isOwnerByUsername(req, res, next) {
-  const { username } = req.params;
+  const { id } = req.params;
 
-  if (!req.user || req.user.username !== username) {
+  if (!req.user || req.user._id.toString() !== id) {
     return res.status(403).json({ message: "Access denied: Not your profile" });
   }
 
